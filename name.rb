89b983034bdefab0371b7a0ec0ba1f652d47b1fe
Zacharae <zacharyxae@gmail.com>
2016-04-14 08:55:20 -0500

    adds math problems, brought to you by the color blue

--- name.rb
+++ name.rb
@@ -1,4 +1,4 @@
-# 1
+# # 1
 puts "what's your first name?"
 first_name = gets.chomp
 puts "your last name?"
@@ -7,3 +7,57 @@ puts "#{first_name} #{last_name} there are #{first_name.length + last_name.lengt
 
 #2
 #puts "OK #{first_name}, let's do some math"
+
+def maths
+	puts "2+2= ?"
+	four = gets.chomp.to_i
+	if four == 4
+		puts "very good"
+	else
+		puts "try harder"
+		maths
+	end
+end
+
+maths
+
+def maths2
+	puts "4+4= ?"
+	eight = gets.chomp.to_i
+	if eight == 8
+		puts "very good"
+	else
+		puts "try harder"
+		maths2
+	end
+end
+
+maths2
+
+def maths3
+	puts "8+8= ?"
+	sixteen = gets.chomp.to_i
+	if sixteen == 16
+		puts "very good"
+	else
+		puts "try harder"
+		maths3
+	end
+end
+
+maths3
+
+def colortest
+	puts "name a color"
+	coloris = gets.chomp.to_s
+	if coloris == "blue" || coloris == "green"
+		puts "very good"
+	else
+		puts "try harder"
+		colortest
+	end
+end
+
+colortest
+
+
