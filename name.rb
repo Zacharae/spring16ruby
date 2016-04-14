b1aabf6416f612656b1b3236133d9031b05f2ee8
Zacharae <zacharyxae@gmail.com>
2016-04-13 23:26:53 -0500

    name letter count 1

---
+++ name.rb
@@ -0,0 +1,9 @@
+# 1
+puts "what's your first name?"
+first_name = gets.chomp
+puts "your last name?"
+last_name = gets.chomp
+puts "#{first_name} #{last_name} there are #{first_name.length + last_name.length} characters in your name."
+
+#2
+#puts "OK #{first_name}, let's do some math"
